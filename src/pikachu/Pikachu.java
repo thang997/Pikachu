13879155b56fa6c4b1bb6e21c10686296777b4fe
Fuck <nguyenthang9720@gmail.com>
2018-10-26 19:23:23 +0700

    gqgq

---
+++ src/pikachu/Pikachu.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pikachu;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterJob;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Thang
+ */
+public class Pikachu extends Canvas{  
+      
+    public void paint(Graphics g) {  
+        g.drawString("Hello",40,40);  
+        setBackground(Color.WHITE);  
+        g.fillRect(130, 30,100, 80);  
+        g.drawOval(30,130,50, 60);  
+        setForeground(Color.RED);  
+        g.fillOval(130,130,50, 60);  
+        g.drawArc(30, 200, 40,50,90,60);  
+        g.fillArc(30, 130, 40,50,180,40);  
+          
+    }  
+        public static void main(String[] args) {  
+        Pikachu m=new Pikachu();  
+        JFrame f=new JFrame();  
+        f.add(m);  
+        f.setSize(400,400);  
+        //f.setLayout(null);  
+        f.setVisible(true);  
+    }  
+  
+}  
